15eec38d83acc85ea9ebebd0415e88dbee0aebad
Noel Escobedo <noel_ska15@hotmail.com>
2016-01-12 20:54:48 -0600

    Bootstrap application

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.set('view cache', false);
+swig.setDefaults({ cache: false });
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
