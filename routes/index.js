7f4ab586153e0b15b7ab20745dd51505403f6daa
AdanCruzHuerta <adancruzhuerta@gmail.com>
2016-01-16 12:42:52 -0600

    Envio de datos de expedientes a backend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('form/expediente', { title: 'Express' });
+});
+
+/*POST Form*/
+router.post('/save', function(req, res, next){
+	console.log(req);
+	res.write("datos enviados");
 });
 
 module.exports = router;
