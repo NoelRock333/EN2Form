c6b9ebf0f3fdb2b3ceac2e3c457295969cc72790
Noel Escobedo <noel_ska15@hotmail.com>
2016-01-16 01:49:27 -0600

    Agregado massive para conectar base de datos postgres Creado formulario de ejemplo para insersión de datos

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('form/expediente', { title: 'Express' });
 });
 
+router.get('/index/ejemplo', function(req, res, next) {
+  res.render('form/ejemplo', { title: 'Formulario Ejemplo' });
+});
+
+router.post('/index/save-form', function(req, res, next) {
+  var db = req.app.get('db');
+  db.ma_endodoncia.insert(req.body, function(err, data){
+    if(err) return res.send(err);
+    res.send(req.body);
+  });
+});
+
 module.exports = router;
