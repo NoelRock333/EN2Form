56dbb6e8ea0ab46f24cea8cc8e637313b24aaaa9
Noel Escobedo <noel_ska15@hotmail.com>
2016-01-17 21:14:26 -0600

    Modificados campos preparando para insertar

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var extend = require('util')._extend;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,8 +10,27 @@ router.get('/', function(req, res, next) {
 
 /*POST Form*/
 router.post('/save', function(req, res, next){
-	console.log(req);
-	res.write("datos enviados");
+	var db = req.app.get('db');
+	var expediente = {
+		ids_problemas: req.body.problemas,
+		ids_alergias: req.body.alergias,
+		ids_antecedentes_del_diente: req.body.antecendentes_diente,
+		otro_antecedentes_del_diente: req.body.otro_antecedente,
+		ids_examen_clinico: req.body.examen_clinico,
+		ids_pulpa: req.body.pulpa,
+		ids_palpitacion_periapcial: req.body.palpacion_periapical,
+		ids_conducto_radicular_rx: req.body.conducto_radicular,
+		ids_zona_periapcial_rx: req.body.zona_periapical,
+		ids_diagnostico_pulpar: req.body.diagnostico_pulpar,
+		ids_diagnostico_periapcial_presuncion: req.body.diagnostico_periapical_presuncion,
+		ids_interencion_indicada: req.body.intereccion_indicada
+	};
+	expediente = extend(expediente,req.body);
+
+	db.ma_endodoncia.insert(expediente, function(err, data){
+		if(err) return res.send(err);
+		res.send(data);
+	});
 });
 
 router.get('/index/ejemplo', function(req, res, next) {
--- sql/tables.sql
+++ sql/tables.sql
@@ -14,11 +14,30 @@ WITH (
 DROP TABLE IF EXISTS "public"."ma_endodoncia";
 CREATE TABLE "public"."ma_endodoncia" (
 	id bigserial NOT NULL,
-	id_paciente character varying,
-	fecha date,
-	id_referencia character varying,
+	id_paciente integer,
+	anestecia_previa boolean,
+	fecha_expediente date,
+	id_referencia integer,
+	edad_paciente integer,
+	piezas_dentales integer[],
 	ids_alergias integer[],
-	enfermedad_dolor character varying
+	otra_alergia character varying,
+	enfermedad_dolores character varying,
+	ultimos_medicamentos character varying,
+	ids_problemas integer[],
+	ids_antecedentes_del_diente integer[],
+	otro_antecedentes_del_diente character varying,
+	ids_examen_clinico integer[],
+	ids_pulpa integer[],
+	ids_palpitacion_periapcial integer[],
+	ids_conducto_radicular_rx integer[],
+	ids_zona_periapcial_rx integer[],
+	ids_diagnostico_pulpar integer[],
+	ids_diagnostico_periapcial_presuncion integer[],
+	ids_interencion_indicada integer[],
+	control_de_tratamiento integer[][],
+	pronostico boolean,
+	nota_evolucion character varying
 )
 WITH (OIDS=FALSE);
 ALTER TABLE "public"."ma_endodoncia" ADD PRIMARY KEY ("id");
