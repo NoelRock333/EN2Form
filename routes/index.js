2fe775cf7795dc01e9c30c3342297c2079bf3843
Noel Escobedo <noel_ska15@hotmail.com>
2016-01-30 01:06:40 -0600

    Agregadas listas de pacientes y colegas Agregados formularios de edicion de pacientes y colegas

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ var passwordHash = require('password-hash');
 
 //Formulario para llenar expediente de Endodocia
 router.get('/', utils.requireAuthorization, function(req, res, next) {
+  res.render('index', { title: 'Express', user: req.session.user });
+});
+
+router.get('/expediente', utils.requireAuthorization, function(req, res, next) {
   res.render('form/expediente', { title: 'Express', user: req.session.user });
 });
 
@@ -118,7 +122,7 @@ router.post('/nuevo_paciente', function(req, res, next){
 	req.body.fecha_nacimiento = req.body.fecha_nacimiento ? moment(req.body.fecha_nacimiento, 'DD/MM/YYYY').format('YYYY-MM-DD') : null,
 	req.body.nombre_completo = `${req.body.nombre} ${req.body.apellido_paterno} ${req.body.apellido_materno}`.trim();
 	db.ca_pacientes.insert(req.body, function(err, data){
-		if(err) return res.send(err);
+		if(err) return res.json(err);
 		res.json(data);
 	});
 });
@@ -129,27 +133,83 @@ router.post('/nuevo_colega', function(req, res, next){
 	req.body.apellido_paterno 	= req.body.apellido_paterno || null;
 	req.body.nombre_completo 	= `${req.body.nombre} ${req.body.apellido_paterno} ${req.body.apellido_materno}`.trim();
 	db.ca_colegas.insert(req.body, function(err, data){
-		if(err) return res.send(err);
+		if(err) return res.json(err);
 		res.json(data);
 	});
 });
 
+
+
+
+router.get('/colegas', function(req, res, next) {
+	var db = req.app.get('db');
+	db.ca_colegas.find({}, function(err, data){
+		if(err) return res.send(err);
+		res.render('tables/colegas', { user: req.session.user, colegas: data });
+	});
+});
 
 router.post('/colegas', function(req, res, next) {
 	var db = req.app.get('db');
 	db.run("SELECT * FROM ca_colegas WHERE lower(nombre_completo) LIKE '%"+req.body.query+"%'", function(err, data){
-		if(err) return res.send(err);
+		if(err) return res.json(err);
 		res.json(data);
 	});
 });
 
-router.post('/pacientes', function(req, res, next) {
+router.get('/colega/:id', function(req, res, next) {
 	var db = req.app.get('db');
-	db.run("SELECT * FROM ca_pacientes WHERE lower(nombre_completo) LIKE '%"+req.body.query+"%'", function(err, data){
+	db.ca_colegas.findOne({ id: req.params.id }, function(err, data){
 		if(err) return res.send(err);
+		res.render('editors/colega', { user: req.session.user, colega: data });
+	});
+});
+
+router.put('/colega', function(req, res, next) {
+	var db = req.app.get('db');
+	db.ca_colegas.update(req.body, function(err, data){
+		if(err) return res.json(err);
 		res.json(data);
 	});
 });
+
+
+
+router.get('/pacientes', function(req, res, next) {
+	var db = req.app.get('db');
+	db.ca_pacientes.find({}, function(err, data){
+		if(err) return res.send(err);
+		res.render('tables/pacientes', { user: req.session.user, pacientes: data });
+	});
+});
+router.post('/pacientes', function(req, res, next) {
+	var db = req.app.get('db');
+	db.run("SELECT * FROM ca_pacientes WHERE lower(nombre_completo) LIKE '%"+req.body.query+"%'", function(err, data){
+		if(err) return res.json(err);
+		res.json(data);
+	});
+});
+
+router.get('/paciente/:id', function(req, res, next) {
+	var db = req.app.get('db');
+	db.ca_pacientes.findOne({ id: req.params.id }, function(err, data){
+		if(err) return res.send(err);
+		data.fecha_nacimiento = (data.fecha_nacimiento) ? moment(data.fecha_nacimiento).format('DD/MM/YYYY').toString() : '';
+		res.render('editors/paciente', { user: req.session.user, paciente: data });
+	});
+});
+
+router.put('/paciente', function(req, res, next) {
+	var db = req.app.get('db');
+	req.body.fecha_nacimiento = (req.body.fecha_nacimiento) ? moment(req.body.fecha_nacimiento, 'DD/MM/YYYY').format('YYYY-MM-DD') : null;
+	db.ca_pacientes.update(req.body, function(err, data){
+		if(err) return res.json(err);
+		res.json(data);
+	});
+});
+
+
+
 
 router.get('/index/ejemplo', function(req, res, next) {
   res.render('form/ejemplo', { title: 'Formulario Ejemplo' });
@@ -164,13 +224,3 @@ router.post('/index/save-form', function(req, res, next) {
 });
 
 module.exports = router;
-
-function dateFormat(fecha){
-	if(fecha){
-		var date = fecha.split('/');
-		return date[2]+'-'+date[0]+'-'+date[1];
-	}
-	else{
-		return "00-00-0000";
-	}
-}
